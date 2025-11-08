2c9d1a9f2ca8130f4da6ef271c7077e125402d56
hassu2010 <hassanalitagar537@gmail.com>
2025-11-08 20:36:54 +0500

    Descriptive commit message

---
+++ lecture-4/index.js
@@ -0,0 +1,23 @@
+let num1=prompt("Enter a first digit");
+Number(num1)
+let num2=prompt("Enter a second digit");
+Number(num2)
+console.log(num1 + num2); 
+console.log(num1 - num2); 
+console.log(num1 * num2);
+console.log(num1 / num2); 
+console.log(num1 % num2); 
+console.log(num1 ** num2); 
+
+console.log('-----------------------Second topic--------------------');
+let userEmail=prompt('enter a email');
+let userPassword=prompt('enter a password');
+let savedEmail = "student@gmail.com";
+let savedPassword = "12345";
+console.log(userEmail == savedEmail && userPassword == savedPassword);
+console.log("Login Successful:", userEmail == savedEmail && userPassword == savedPassword);
+console.log('------------------------Third topic-------------------------');
+let count = 0;
+console.log(++count); 
+console.log(++count); 
+console.log(--count); 
