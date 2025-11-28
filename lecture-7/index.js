f8c9ee9d304478c5859e600508e1d16b8a984ccc
hassu2010 <hassanalitagar537@gmail.com>
2025-11-28 10:31:34 +0500

    Function assigment

--- lecture-7/index.js
+++ lecture-7/index.js
@@ -1,15 +1,23 @@
 console.log("--------------------------------------------Topic 1-----------------------------------------------------");
   
 
-let name= prompt("Enter your Name")
-function greet(name) {
- console.log("Hello " + name);
-}
-greet(name)
+// let name= prompt("Enter your Name")
+// function greet(name) {
+//  console.log("Hello " + name);
+// }
+// greet(name)
  
-let num1 = +prompt("Enter you First Number")
-let num2 = +prompt("Enter you Sencond Number")
-function sum(num1,num2) {
-console.log (num1+num2);
+// let num1 = +prompt("Enter you First Number")
+// let num2 = +prompt("Enter you Sencond Number")
+// function sum(num1,num2) {
+// console.log (num1+num2);
+// }
+// sum(num1,num2)
+
+
+let luckyname= prompt("Enter you lucky Name")
+let luckynumber= +prompt("Enter you lucky number")
+function generator(luckyname,luckynumber) {
+    alert(luckyname +" "+ "your lucky number is" +" "+ luckynumber);
 }
-sum(num1,num2)
+generator(luckyname,luckynumber)
