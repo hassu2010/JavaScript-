c0951c4f7cde1c68e6f8b1d1ea0507522728925f
hassu2010 <hassanalitagar537@gmail.com>
2025-11-28 10:22:29 +0500

    loop assigment

---
+++ lecture-7/index.js
@@ -0,0 +1,15 @@
+console.log("--------------------------------------------Topic 1-----------------------------------------------------");
+  
+
+let name= prompt("Enter your Name")
+function greet(name) {
+ console.log("Hello " + name);
+}
+greet(name)
+ 
+let num1 = +prompt("Enter you First Number")
+let num2 = +prompt("Enter you Sencond Number")
+function sum(num1,num2) {
+console.log (num1+num2);
+}
+sum(num1,num2)
